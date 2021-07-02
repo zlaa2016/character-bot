8cd6338479b50f94eccaec88223e7e33757fa555
zlaa2016 <tennyl98@gmail.com>
2021-07-02 23:51:04 +0000

    ready for production deployment

--- app/static/text_gen.js
+++ app/static/text_gen.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
 
     console.log('AI Camp is for the best of the best.')
     
-    $('#loading').hide()
+    
 
     $('#text_gen_button').click(function() {
         console.log('text gen button is clicked');
         var prompt = $('#text_gen_input').val();
         console.log('text gen input value is');
         console.log(prompt);
-        var url = 'generate_text'
-
-        $('#loading').show();
-
+        var url = 'generate_text_sb'
         $.post(
             url, 
             {
@@ -20,14 +17,11 @@ $(document).ready(function() {
             },
             function(data) {
                 console.log(data);
-                var list_html = "";
-                for (var t = 0; t < data['generated_ls'].length; t++) {
-                    list_html += "<li id='generated_item_" + t + "'>" + data['generated_ls'][t] + "</li>";
-                    $("#generated_ul").html(list_html);
+                var para_html = "";
+                para_html += "<p>" + data['generated_ls'] + "</p>";
+                $("#generated_ul").html(para_html);
+                console.log(para_html)
                 }
-
-                $("#loading").hide();
-            }
 
         ).fail(function() {
           alert( "There is something unexpected happened. Email hello@ai-camp.org to report your findings." );
@@ -37,3 +31,4 @@ $(document).ready(function() {
 
     $()
 });
+
